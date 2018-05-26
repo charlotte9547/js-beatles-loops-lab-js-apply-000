07cabfeea221b04ac6bc84b1308854ce1ba3a873
Xingxin Chen <xingxinc@andrew.cmu.edu>
2018-05-26 18:06:53 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function theBeatlesPlay (musicians, instruments) {
+  const array = []
+  for (let i = 0 ; i = musicians.length ; i ++){
+    array.push(`[musicians] plays [instruments]`)
+      
+  }
+  return array
+}
